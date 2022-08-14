52e4bfd301be42c83bf6ad5b8ad7daa1e0e513d2
Emily Spradling <spradling.ed@gmail.com>
2022-08-14 11:24:54 -0500

    exercises completed

--- exercises/parts1-5.ts
+++ exercises/parts1-5.ts
@@ -1,12 +1,14 @@
 // URL for the instructions: 
 // https://education.launchcode.org/intro-to-professional-web-dev/chapters/typescript/exercises.html 
 
-
+import { SpaceLocation } from './SpaceLocation';
 // Part 1: Declare (5) Variables With Type
-
+let kilometersToMars: number = 225000000;
+let kilometersToTheMoon: number = 384400;
 
 
 // Part 2: Print Days to Mars
+
 
 
 
@@ -25,7 +27,27 @@
 
 
 // Part 4: Create a Spacecraft Class
+class Spacecraft {
+    milesPerKilometer: number = 0.621;
+    name: string;
+    speedMph: number;
+    constructor(name: string, speedMph: number) {
+        this.name = name;
+        this.speedMph = speedMph;
+    }
+    getDaysToLocation(kilometersAway: number): number {
+        let milesAway: number = kilometersAway * this.milesPerKilometer;
+        let hoursToLocation: number = milesAway / this.speedMph;
+        return hoursToLocation / 24;
+    }
+    printDaysToLocation(location: SpaceLocation) {
+        console.log(`${this.name} would take ${this.getDaysToLocation(location.kilometersAway)} days to get to ${location.name}.`);
+     }
+}
+let spaceShuttle = new Spacecraft('Determination', 17500);
 
+spaceShuttle.printDaysToLocation(new SpaceLocation('Mars', kilometersToMars));
+spaceShuttle.printDaysToLocation(new SpaceLocation('the Moon', kilometersToTheMoon));
 
 
 
